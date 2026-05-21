ef421940389c99553b708953598f5b2cd2c5c270
kozh-k <kozhemyachenkokirill05@gmail.com>
2026-05-21 05:57:55 +0300

    add chats to redux

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import profileReducer from "./profile/profileSlice";
+import chatsReducer from "./chats/chatsSlice";
 
 export const store = configureStore({
    reducer: {
       profile: profileReducer,
+      chats: chatsReducer,
    },
 });
